efffd7ab288a8f09939a71d7134e8cc3b11b4c2e
Kevin Boston <kbos22@gmail.com>
2020-07-06 17:50:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,4 +89,14 @@ class Student
     SQL
     DB[:conn].execute(sql,num).map {|row|self.new_from_db(row)}
   end
+  def self.first_student_in_grade_10
+    sql = <<-SQL 
+    SELECT *
+    FROM students
+    WHERE grade = 10 
+    ORDER BY students.id 
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map {|row|self.new_from_db(row)}
+  end
 end
